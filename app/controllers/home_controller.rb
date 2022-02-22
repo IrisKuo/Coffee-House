f28b7e33d30e0a8e14d07b3b94384a07f10755be
Iris Kuo <iriskuo0523@gmail.com>
2022-02-22 15:17:31 +0800

    add jquery & add bootstrap

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index  
+   @nav_flag = false
+  end
+
+  def first
+    @nav_flag = true
+  end
+
+  def show_post
+    @nav_flag = true
+
+    @testName = params[:id]
+  end
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,18 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import jQuery from "./jquery-3.6.0.min"
+window.$ = window.jQuery = jQuery
+
+import Proper from "./bootstrap5/popper.min"
+import Bootstrap from "./bootstrap5/bootstrap.min"
+
+
+
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
