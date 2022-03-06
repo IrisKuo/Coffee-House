52eb69db1fa0d9b8e281533e5475216512efebc0
Iris Kuo <iriskuo0523@gmail.com>
2022-03-06 21:47:17 +0800

    change product for each

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,36 +6,47 @@ class HomeController < ApplicationController
       des: "這是廣告!!",
       action_title: "詳細內容"
     }
-    @products = 
-      {
-      id: "1",
-      name: "美式咖啡",
-      description: "阿拉比卡咖啡豆",
-      image_url: "/images/Americano.jpegs",
-      }
-      # {
-      # id: "2",
-      # name: "拿鐵",
-      # description: "拉丁美洲咖啡豆",
-      # image_url: "/images/Americano.jpegs",
-      # }
-    
-      # {
-      #   id: "3",
-      #   name: "卡布奇諾",
-      #   description:"瓜地馬拉咖啡豆",
-      #   image_url: "/images/Americano.jpegs",
-      # },
-      # {
-      #   id: "4",
-      #   name: "焦糖瑪奇朵",
-      #   description:"耶加雪菲咖啡豆",
-      #   image_url: "/images/Americano.jpegs",
-      # }
-    
 
-      # @ad_title = "大型廣告!!" 
-      # @ad_des = "這是廣告!!"
-      # @ad_action_title = "詳細內容"
+    # @products = []
+    # [1, 2, 3, 4,].each do |index|
+    #   product ={
+    #     id:"index",
+    #     name:"美式咖啡",
+    #     description:"阿拉比卡咖啡豆"
+    #   }
+    #   @products << product
+    # end
+
+
+    @products = [
+    {
+      id:"1",
+      name:"美式咖啡",
+      description:"阿拉比卡咖啡豆",
+      image_url:"/images/Americano.jpeg",
+    },
+    {
+      id: "2",
+      name: "拿鐵",
+      description: "拉丁美洲咖啡豆",
+      image_url: "/images/cappuccino.jpeg",
+    },
+    {
+      id: "3",
+      name: "卡布奇諾",
+      description:"瓜地馬拉咖啡豆",
+      image_url: "/images/latte.jpeg",
+    },
+    {
+      id: "4",
+      name: "焦糖瑪奇朵",
+      description:"耶加雪菲咖啡豆",
+      image_url: "/images/Macchiato.jpeg",
+    }
+    ]
+
+      @ad_title = "大型廣告!!" 
+      @ad_des = "這是廣告!!"
+      @ad_action_title = "詳細內容"
   end 
 end
